ff1dda72063bcae4a359973df97a6b49f8829ad4
GitHub Action <action@github.com>
2026-04-27 21:36:39 +0000

    Add cardPresso to supported apps list

--- .github/scripts/collect_app_info.py
+++ .github/scripts/collect_app_info.py
@@ -1214,6 +1214,7 @@ homebrew_cask_urls = [
     "https://formulae.brew.sh/api/cask/finetune.json",
     "https://formulae.brew.sh/api/cask/heynote.json",
     "https://formulae.brew.sh/api/cask/morgen.json",
+    "https://formulae.brew.sh/api/cask/cardpresso.json",
 ]
 
 # PKG in DMG URLs
